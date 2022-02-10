6bc9ddd7faf3e4b7e6ac61d8517c3bce2f2d33ab
Danyi Zhang <danyi.zhang@cern.ch>
2022-02-10 22:45:27 +0800

    add helloworld program

---
+++ Session1/helloworld.cpp
@@ -0,0 +1,15 @@
+  #include<iostream>
+  using namespace std;
+
+  int main()
+  {
+    // Core program
+    cout << "Hello World!" << endl;
+
+    // Wait that user pushes a button (relevant for Windows system)
+    cout << "Push a button to quit" << endl;
+    cin.get();
+    
+    // Return no error code
+    return 0;
+  }
